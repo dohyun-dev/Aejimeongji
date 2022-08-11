f8a6992de3223f9887175f90f3dba32957040fed
권도현 <tobeykwon@gmail.com>
2022-08-11 17:37:57 +0900

    refact : null 처리 수정 (#71)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/petPlace/PetPlaceResponse.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/dto/petPlace/PetPlaceResponse.java
@@ -3,7 +3,9 @@ package com.ssafy.aejimeongji.api.dto.petPlace;
 import com.ssafy.aejimeongji.domain.entity.PetPlace;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Math.*;
@@ -41,11 +43,12 @@ public class PetPlaceResponse {
         rating = petPlace.getRating();
         openingHours = petPlace.getOpeningHours();
         distance = (6371 * acos(cos(toRadians(lat)) * cos(toRadians(petPlace.getPoint().getX())) * cos(toRadians(petPlace.getPoint().getY()) - toRadians(lng)) + sin(toRadians(lat)) * sin(toRadians(petPlace.getPoint().getX()))));
-        petplaceImageUrl = List.of(petPlace.getPetplaceImageSet().getPetplaceImage().split(","));
-        petplaceInfoUrl = List.of(petPlace.getPetplaceImageSet().getPetplaceInfo().split(","));
-        petplaceMenuUrl = List.of(petPlace.getPetplaceImageSet().getPetplaceMenu().split(","));
+        petplaceImageUrl = getImageList(petPlace.getPetplaceImageSet().getPetplaceImage());
+        petplaceInfoUrl = getImageList(petPlace.getPetplaceImageSet().getPetplaceInfo());
+        petplaceInfoUrl = getImageList(petPlace.getPetplaceImageSet().getPetplaceMenu());
         petplaceThumbnail = petplaceImageUrl.get(0);
     }
+
     public PetPlaceResponse(PetPlace petPlace) {
         id = petPlace.getId();
         name = petPlace.getName();
@@ -60,5 +63,13 @@ public class PetPlaceResponse {
         petplaceInfoUrl = List.of(petPlace.getPetplaceImageSet().getPetplaceInfo().split(","));
         petplaceMenuUrl = List.of(petPlace.getPetplaceImageSet().getPetplaceMenu().split(","));
         petplaceThumbnail = petplaceImageUrl.get(0);
+    }
+
+    private List<String> getImageList(String image) {
+        if (StringUtils.hasText(image)) {
+            return List.of(image.split(","));
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
