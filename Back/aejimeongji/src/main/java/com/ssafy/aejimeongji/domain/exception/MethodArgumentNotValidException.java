7dbcf2dea684016f6ddf06d26156a527c14764d2
TobeyKwon <tobeykwon@gmail.com>
2022-08-13 19:34:26 +0900

    feat : ValidationException 추가 (#79)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/exception/MethodArgumentNotValidException.java
@@ -0,0 +1,27 @@
+package com.ssafy.aejimeongji.domain.exception;
+
+import lombok.Data;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class MethodArgumentNotValidException extends RuntimeException {
+    private BindingResult bindingResult;
+
+    public Map<String, String> makeErrors() {
+        HashMap<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors().stream()
+                .forEach(e -> {
+                        if (e instanceof FieldError) {
+                            errors.put(((FieldError) e).getField(), e.getDefaultMessage());
+                        } else {
+                            errors.put(e.getObjectName(), e.getDefaultMessage());
+                        }
+                    }
+                );
+        return errors;
+    }
+}
