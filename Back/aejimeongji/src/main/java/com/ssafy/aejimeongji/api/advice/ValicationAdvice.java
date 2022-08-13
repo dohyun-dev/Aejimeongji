b796d859c4352fc8458772b97de183dbc1ae42ed
TobeyKwon <tobeykwon@gmail.com>
2022-08-13 19:30:52 +0900

    feat : ValidationExceptionHandler 추가 (#79)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/advice/ValicationAdvice.java
@@ -0,0 +1,11 @@
+package com.ssafy.aejimeongji.api.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ValicationAdvice {
+
+}
