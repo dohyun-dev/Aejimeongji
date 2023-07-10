18a9f84647fbae6ffeba755c66f97602f64b961f
권도현 <tobeykwon@gmail.com>
2023-07-10 11:49:55 +0900

    fix : 배포 스크립트 오류 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,12 @@
 DOCKER_IMAGE_NAME=tobeykwon/aejimeongji-server
+DOCKER_CONTAINER_NAME=server
 
 echo "도커 이미지 다운"
 docker pull $DOCKER_IMAGE_NAME
 
 echo "기존 컨테이너 종료 및 삭제"
-docker stop $DOCKER_IMAGE_NAME
-docker rmi -f $DOCKER_IMAGE_NAME
+docker stop $DOCKER_CONTAINER_NAME
+docker rm $DOCKER_CONTAINER_NAME
 
 echo "도커 컨테이너 실행"
-docker run -itd -p 8080:8080 $DOCKER_IMAGE_NAME
+docker run -itd -p 8080:8080 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
