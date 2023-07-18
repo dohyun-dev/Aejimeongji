4dd372bcd392b97ed42c1e6ae4998a52b1e08ff9
권도현 <tobeykwon@gmail.com>
2023-07-18 14:02:45 +0900

    rename: FileUtil -> FileUtils 이름 변경 #11

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/util/FileUtils.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/util/FileUtils.java
@@ -1,18 +1,17 @@
 package com.ssafy.aejimeongji.domain.util;
 
-import com.ssafy.aejimeongji.api.dto.FileDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
 
-public interface FileUtil {
+public interface FileUtils {
 
-    FileDto storeImage(MultipartFile multipartFile) throws IOException;
+    String storeImage(MultipartFile multipartFile) throws IOException;
     void deleteFile(String storeFileUrl);
 
     default String createStoreFilename(String originalFilename) {
-        return UUID.randomUUID().toString() + "." + extractExt(originalFilename);
+        return UUID.randomUUID() + "." + extractExt(originalFilename);
     }
 
     default String extractExt(String originalFilename) {
