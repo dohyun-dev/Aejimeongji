aa24c3ccfdcd86e8b7bc6d3f3627eac8750b969c
권도현 <tobeykwon@gmail.com>
2023-08-02 18:13:27 +0900

    feat: PhoneAuthApiControllerV2 추가 #22

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/auth/presentation/PhoneAuthApiControllerV2.java
@@ -0,0 +1,41 @@
+package com.ssafy.aejimeongji.domain.auth.presentation;
+
+import com.ssafy.aejimeongji.domain.auth.application.dto.PhoneAuthSendRequest;
+import com.ssafy.aejimeongji.domain.auth.application.dto.PhoneAuthSendResponse;
+import com.ssafy.aejimeongji.domain.auth.application.dto.PhoneAuthVerifyRequest;
+import com.ssafy.aejimeongji.domain.auth.application.service.PhoneAuthService;
+import com.ssafy.aejimeongji.domain.common.application.dto.ResponseDTO;
+import com.ssafy.aejimeongji.domain.common.exception.CustomError;
+import com.ssafy.aejimeongji.domain.common.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v2/phone-auth")
+@RequiredArgsConstructor
+public class PhoneAuthApiControllerV2 {
+
+    private final PhoneAuthService phoneAuthService;
+
+    @PostMapping("/verify")
+    public ResponseEntity<ResponseDTO> confirmAuthNumber(@RequestBody PhoneAuthVerifyRequest request) {
+        boolean result = phoneAuthService.verifyAuthNumber(request.getPhoneUUID(), request.getAuthNumber());
+        if (!result)
+            throw new CustomException(CustomError.PHONE_AUTH_FAILURE);
+        return ResponseEntity.ok().body(new ResponseDTO("인증이 완료되었습니다."));
+    }
+
+    @PostMapping
+    public ResponseEntity<PhoneAuthSendResponse> sendMessage(@Valid @RequestBody PhoneAuthSendRequest request) {
+        String phoneUUID = phoneAuthService.sendMessage(request.getRecipientNumber());
+        return ResponseEntity.ok().body(new PhoneAuthSendResponse("인증번호가 발송되었습니다.", phoneUUID));
+    }
+}
