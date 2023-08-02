374f62a39808e7a945f5141f69061577e0315d50
권도현 <tobeykwon@gmail.com>
2023-08-02 18:05:40 +0900

    feat: KafkaProperties 추가 #22

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/global/util/KafkaProperties.java
@@ -0,0 +1,7 @@
+package com.ssafy.aejimeongji.global.util;
+
+public interface KafkaProperties {
+    String SEND_MESSAGE = "send_message";
+    String CONSUMER_GROUP_ID = "spring-app";
+    String DEFAULT_THREAD_COUNT = "2";
+}
